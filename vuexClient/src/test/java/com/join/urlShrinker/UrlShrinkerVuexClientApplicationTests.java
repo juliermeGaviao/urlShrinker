cd0f45427850e2ba8a7a245c87b181e70fee8e04
Julierme <Julierme@DESKTOP-4V5BP5T>
2019-04-16 20:32:50 -0300

    Test classe renamed

--- vuexClient/src/test/java/com/join/urlShrinker/UrlShrinkerVuexClientApplicationTests.java
+++ vuexClient/src/test/java/com/join/urlShrinker/UrlShrinkerVuexClientApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UrlShrinkerVueClientApplicationTests {
+public class UrlShrinkerVuexClientApplicationTests {
 
 	@Test
 	public void contextLoads() {
